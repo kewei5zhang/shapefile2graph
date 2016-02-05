1bf0198b12443137a60dda363a4a484c7b376cfa
Kewei Zhang <s4366844@labs.eait.uq.edu.au>
2016-02-05 18:05:11 +1000

    final edit 5/02/2016

--- src/main/java/shapefile/graph/DemoGraph.java
+++ src/main/java/shapefile/graph/DemoGraph.java
@@ -1,8 +1,11 @@
 package shapefile.graph;
 import java.util.List;
 
-
-
+/**
+ * the DemoGraph class contains the main method
+ * @author s4366844 Kewei Zhang
+ *
+ */
 public class DemoGraph{
 	
 	public static void main(String[] args) throws Exception {
@@ -14,14 +17,10 @@ public class DemoGraph{
 		//compute shortest path
 		String source = "173036";
 		String target = "273509";
-    DijkstraAlgorithm.computePaths((Vertex)dGraph.getShapeFileNodes().get(source));
-    System.out.println("Distance from " + source + " to " + target + " : " + dGraph.getShapeFileNodes().get(target).minDistance);
-    List<Vertex> path = DijkstraAlgorithm.getShortestPathTo(dGraph.getShapeFileNodes().get(target));
-    System.out.println("Path:" + path);
-    for(int i = 0; i<= path.size()-1;i++){
-
-    	System.out.println(path.get(i) );
-}
+		DijkstraAlgorithm.computePaths((Vertex)dGraph.getShapeFileNodes().get(source));
+		System.out.println("Distance from " + source + " to " + target + " : " + dGraph.getShapeFileNodes().get(target).minDistance);
+		List<Vertex> path = DijkstraAlgorithm.getShortestPathTo(dGraph.getShapeFileNodes().get(target));
+		System.out.println("Path:" + path);
 
 }}
 
